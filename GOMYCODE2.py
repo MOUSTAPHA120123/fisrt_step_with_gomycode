133a5d47493cd1bb560a78ab2331344db9abe255
MOUSTAPHA120123 <zafbodiang@gmail.com>
2024-08-12 09:17:09 +0000

    Add files via upload

---
+++ GOMYCODE2.py
@@ -0,0 +1,38 @@
+# EXERCICE PRATIQUE
+
+# Invitation au jeu
+print("Bienvenue dans le jeu Devinez le nombre !")
+print("Je pense à un nombre compris entre 1 et 150. Pouvez-vous deviner ce que c’est ?")
+
+# Entrée de la réponse
+supposition= 55
+input("saisir votre supposition ?")
+int(input("saisir votre supposition ?"))
+
+#Importation de random
+
+from random import *
+supposition= randint(1,150)
+print(supposition)
+
+# Expression if else
+supposition==55
+
+if supposition ==55:
+    print ('Bravo bien joué !!!')
+
+else:
+    print('Essayer encore')
+
+#Boucle while
+
+while supposition !=55:
+    print('Deviner encore SVP')
+
+
+
+
+
+
+
+
